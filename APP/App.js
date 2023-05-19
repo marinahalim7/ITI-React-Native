7df0c3f9e7c015ce5876a8c57687d97c3f6655c4
marinahalim7 <marinahalim7@gmail.com>
2023-05-19 12:18:54 +0300

    lap 1 and 2

---
+++ APP/App.js
@@ -0,0 +1,24 @@
+import { StatusBar, StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+import Profile from "./src/Pages/Profile";
+import Movies from "./src/Pages/Movies";
+import Movie_Details from "./src/Pages/Movie_Details";
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Profile" component={Profile}/>
+          <Stack.Screen name="Movies" component={Movies} />
+          <Stack.Screen name="Movie_Details" component={Movie_Details}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
